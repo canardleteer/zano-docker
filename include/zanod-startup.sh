48bbf1aadfa2e907816ca29963cd195bd557fb7e
canardleteer <eris.has.a.dad+github@gmail.com>
2025-01-13 21:36:04 -0800

    feat: distroless

--- include/zanod-startup.sh
+++ include/zanod-startup.sh
@@ -3,4 +3,4 @@
 # NOTE(canardleteer): Zano isn't quite yet aligned toward environment
 #                     configuration (afaik), so I'm leaving just using
 #                     a broken out script to figure out what I want.
-/usr/bin/zanod
+/usr/bin/zanod $@
